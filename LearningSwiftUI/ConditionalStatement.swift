8e257df2879c2db560a933155ad1cc775564b6ef
suraj kumar <surajkumar@surajs-MacBook-Air.local>
2023-01-31 02:14:14 +0530

    conditional Statement in swiftui

--- LearningSwiftUI/ConditionalStatement.swift
+++ LearningSwiftUI/ConditionalStatement.swift
@@ -6,12 +6,56 @@
 //
 
 import SwiftUI
-
 struct ConditionalStatement: View {
+    @State var showCircle: Bool = false
+    @State var showRectangle: Bool = false
+    
+    @State var isLoadingView: Bool = false
     var body: some View {
-        VStack(spacing: 20) {
-            
+        
+        VStack {
+            Button("Button\(isLoadingView.description)") {
+                isLoadingView.toggle()
+            }
+            if isLoadingView {
+                ProgressView()
+            }
         }
+        
+//        VStack(spacing: 20) {
+//            Button("Circle Button: \(showCircle.description)") {
+//                showCircle.toggle()
+//            }
+//
+//            Button("Rectangle: \(showCircle.description)") {
+//                showRectangle.toggle()
+//            }
+//
+//            if showCircle {
+//                Circle()
+//                    .frame(width: 100, height: 100)
+//
+//            }
+//            if showRectangle{
+//                Rectangle()
+//                    .frame(width: 100, height: 100)
+//            }
+////            if showCircle && showRectangle {
+////
+////                RoundedRectangle(cornerRadius: 25.0)
+////                    .frame(width: 200, height: 100)
+////
+////            }
+//
+//            if showCircle || showRectangle {
+//
+//                RoundedRectangle(cornerRadius: 25.0)
+//                    .frame(width: 200, height: 100)
+//
+//            }
+//            Spacer()
+//
+//        }
     }
 }
 
